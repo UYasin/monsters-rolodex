c8e97b522fb9c7685a1753715e69cd1c9336e05e
mYasin <36346577+UYasin@users.noreply.github.com>
2019-09-17 12:47:50 -0400

    Updates

---
+++ precache-manifest.ceb09f42ae3a0f8978fe10b4f52d9862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec5c29bec8410ee78cde02ebeae9172",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4eab57804210a9b575de",
+    "url": "/monsters-rolodex/static/css/main.f84ff773.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "4eab57804210a9b575de",
+    "url": "/monsters-rolodex/static/js/main.0f64182b.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
